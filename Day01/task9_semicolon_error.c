03689ee65520ba4e50d368b33e7707c6dadccefc
Kale-Francis <kalefrancis989@gmail.com>
2025-08-05 03:39:33 -1200

    I printed a statement without the semicolon at the end and i tried t compile it and it shows an error explaining specifically where the error is coming from and which line of code to look at

--- Day01/task9_semicolon_error.c
+++ Day01/task9_semicolon_error.c
@@ -6,7 +6,7 @@ int main(){
     return 0;
 }
 
-// gcc task9_semicolon_error.c -o program
+//gcc task9_semicolon_error.c -o program
 //task9_semicolon_error.c: In function 'main':
 //task9_semicolon_error.c:4:28: error: expected ';' before 'return'
 //    4 |     printf("Hello, World!")    
