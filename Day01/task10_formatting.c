3dad2de763a5a8f8f4d9d013133e6a95247af8d2
Kale-Francis <kalefrancis989@gmail.com>
2025-08-05 03:58:54 -1200

    I created 3 variables about my favourite 3books of all time, their publication dates, their ratings and printed the information on the console

--- Day01/task10_formatting.c
+++ Day01/task10_formatting.c
@@ -2,10 +2,16 @@
 
 int main(){
     char book1[] = "Harry potter";
-    char book2[] = "50 Laws of Power";
+    int harry_year = 1997;
+    float harry_rating = 4.9;
+    char book2[] = "48 Laws of Power";
+    int power_year = 1998;
+    float power_rating = 4.1;
     char book3[] = "The Art of War";
+    int art_year = 5;
+    float art_rating = 4.0;
 
-    printf("My favourite 3 Books of all time are %s, %s, %s.\n", book1, book2, book3);
+    printf("My favourite 3 Books of all time plus their publication date and their rating are %s, %d, %.1f, %s, %d, %.1f %s, %dth century BC, %.1f.\n", book1, harry_year, harry_rating, book2, power_year, power_rating, book3, art_year, art_rating);
 
     return 0;
 }
