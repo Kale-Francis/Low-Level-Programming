9816ce632521acc596ac7e218efd4f33fcc5bd66
Kale-Francis <kalefrancis989@gmail.com>
2025-08-05 03:58:41 -1200

    I created 3 variables about my favourite 3books of all time, their publication dates, their ratings and printed the information on the console

---
+++ Day01/task10_formatting.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char book1[] = "Harry potter";
+    char book2[] = "50 Laws of Power";
+    char book3[] = "The Art of War";
+
+    printf("My favourite 3 Books of all time are %s, %s, %s.\n", book1, book2, book3);
+
+    return 0;
+}
