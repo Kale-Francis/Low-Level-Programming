bce05ca9be9d5e937d0df323a09a3fee8f7e67d4
Kale-Francis <kalefrancis989@gmail.com>
2025-08-05 04:03:08 -1200

    I created the basic types of variables, assigned them values and printed all of thme in a new line using a single statement

---
+++ Day01/task11_review.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int age = 21;
+    float height = 5.9f;
+    double pi = 3.14159265359;
+    char grade = 'A';
+
+    printf("Age: %d\nHeight: %.1f\nPi: %.11lf\nGrade: %c\n", age, height, pi, grade);
+
+    return 0;
+}
