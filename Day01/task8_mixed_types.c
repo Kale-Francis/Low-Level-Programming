c07c5868ff2e91b7e7893d77ff53b59073e9f9c8
Kale-Francis <kalefrancis989@gmail.com>
2025-08-05 03:33:17 -1200

    I decrlared two variables one is an interger  and the other is a float and stored the result in a float variable and printed the result

---
+++ Day01/task8_mixed_types.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a = 21;
+    float b = 23.5;
+    float sum = a + b;
+
+    printf("The resulted sum between a(%d) and b(%.2f) is %.2f\n", a, b, sum);
+
+    return 0;
+}
