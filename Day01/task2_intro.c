3296735c4c3de4df6a1cac530b8a55807d6ecf8b
Kale-Francis <kalefrancis989@gmail.com>
2025-08-05 02:57:57 -1200

    Created a personal introduction that prints anem, age, and a fun fact about me

---
+++ Day01/task2_intro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char name[] = "Kale Francis";
+    int age = 21;
+    char fun_fact[] = "i like watching animes for fun almost its like a hobby.";
+
+    printf("My name is %s\n", name);
+    printf("My age is %d\n", age);
+    printf("One fun fact about me is that %s\n", fun_fact);
+
+    return 0;
+}
