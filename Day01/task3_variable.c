479db3fa5fda92f78a3b24fa48db0ca25e855c44
Kale-Francis <kalefrancis989@gmail.com>
2025-08-05 03:07:12 -1200

    I decrlared variables for a number, price, and first letter to my name

---
+++ Day01/task3_variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int fav_number = 25;
+    float price = 30.0;
+    char first_letter = 'K';
+
+    printf("My favorite number is %d\n", fav_number);
+    printf("The price is %.2f\n", price);
+    printf("The first letter to my name is %c\n", first_letter);
+
+    return 0;
+}
