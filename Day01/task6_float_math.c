a860258c0a98804974aa38202ca97c729cc36507
Kale-Francis <kalefrancis989@gmail.com>
2025-08-05 03:24:36 -1200

    I decrlared two float variables and their final products and printed the message clearly

---
+++ Day01/task6_float_math.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    float a = 30.4;
+    float b = 24.98;
+    float sum = a + b;
+
+    printf("The sum of %.1f and %.2f is %.2f\n", a, b, sum);
+
+    return 0;
+}
